851d36c6c7ff70af8db391ac56b1c1fe27da90b4
developer-sidani <developer.sidani@gmail.com>
2022-03-23 13:31:36 +0200

    finalizing phone validation

--- src/components/phone-validator/form.jsx
+++ src/components/phone-validator/form.jsx
@@ -4,6 +4,7 @@ import {
 } from 'formik'
 import { PhoneSchema, initialValues } from './formik-handlers'
 import { validatePhone } from '../../api'
+import Modal from './modal'
 
 /**
  * Phone Validator Form Handler Component
@@ -16,17 +17,21 @@ const PhoneValidatorForm = () => {
    * is going to change on submit to show that the request is loading
    */
   const [loading, setLoading] = useState(false)
+  const [open, setOpen] = useState(false)
+  const [data, setData] = useState()
   /**
    * validatePasswordCallback function
    * uses the useCallback function for performance.
    * @param {string} number - The number that needs to be validated
+   * @param {function} callback - Callback function after submit
    * this function will call the api on submit
    */
   const validatePasswordCallback = useCallback(
-    async (number) => {
+    async (number, callback) => {
       try {
         const res = await validatePhone(number)
-        console.log(res)
+        setData(res?.res)
+        callback()
       } catch (err) {
         console.log({ err })
       } finally {
@@ -40,54 +45,61 @@ const PhoneValidatorForm = () => {
     disabled: 'animate-pulse block w-full rounded-md border border-transparent px-5 py-3 bg-gray-400 text-base font-medium text-white shadow sm:px-10',
   }), [])
   return (
-      <Formik
-        initialValues={initialValues}
-        validationSchema={PhoneSchema}
-        onSubmit={({ number }) => {
-          setLoading(true)
-          validatePasswordCallback(number)
-        }}
-      >
-          {({
-            errors, handleSubmit, touched,
-          }) => (
-                <Form
-                  className="mt-12 sm:mx-auto sm:max-w-lg sm:flex"
-                  onSubmit={e => {
-                    e.preventDefault()
-                    handleSubmit()
-                  }}
-                >
-                    <div className="min-w-0 flex-1">
-                        <label htmlFor="phone" className="sr-only">
-                            Phone Number
-                        </label>
-                        <Field
-                          id="phone"
-                          type="text"
-                          name="number"
-                          required
-                          className="block w-full border border-transparent rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-red-600"
-                          placeholder="ex. +961 76 940 820"
-                        />
-                        {errors.number && touched.number ? (
-                            <div className="mt-2 text-pink-900 text-sm">
-                                {errors.number}
-                            </div>
-                        ) : null}
-                    </div>
-                    <div className="mt-4 sm:mt-0 sm:ml-3">
-                        <button
-                          type="submit"
-                          disabled={loading}
-                          className={loading ? buttonClassNames.disabled : buttonClassNames.initial}
-                        >
-                            Check
-                        </button>
-                    </div>
-                </Form>
-          )}
-      </Formik>
+      <>
+           <Modal
+             open={open}
+             setOpen={setOpen}
+             data={data}
+           />
+          <Formik
+            initialValues={initialValues}
+            validationSchema={PhoneSchema}
+            onSubmit={({ number }) => {
+              setLoading(true)
+              validatePasswordCallback(number, () => setOpen(true))
+            }}
+          >
+              {({
+                errors, handleSubmit, touched,
+              }) => (
+                  <Form
+                    className="mt-12 sm:mx-auto sm:max-w-lg sm:flex"
+                    onSubmit={e => {
+                      e.preventDefault()
+                      handleSubmit()
+                    }}
+                  >
+                      <div className="min-w-0 flex-1">
+                          <label htmlFor="phone" className="sr-only">
+                              Phone Number
+                          </label>
+                          <Field
+                            id="phone"
+                            type="text"
+                            name="number"
+                            required
+                            className="block w-full border border-transparent rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-red-600"
+                            placeholder="ex. +961 76 940 820"
+                          />
+                          {errors.number && touched.number ? (
+                              <div className="mt-2 text-pink-900 text-sm">
+                                  {errors.number}
+                              </div>
+                          ) : null}
+                      </div>
+                      <div className="mt-4 sm:mt-0 sm:ml-3">
+                          <button
+                            type="submit"
+                            disabled={loading}
+                            className={loading ? buttonClassNames.disabled : buttonClassNames.initial}
+                          >
+                              Check
+                          </button>
+                      </div>
+                  </Form>
+              )}
+          </Formik>
+      </>
   )
 }
 
